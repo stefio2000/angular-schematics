cc9a89712ec293848b0aabbab1047dac7ae30773
stef <stephane.david@aptar.com>
2019-07-12 11:38:15 +0200

    feat example: modify schematic with templates to create a component with a condition in template

--- demoSchematics/src/demo-template/files/demo-__name@dasherize__/demo-__name@dasherize__.component.ts
+++ demoSchematics/src/demo-template/files/demo-__name@dasherize__/demo-__name@dasherize__.component.ts
@@ -5,11 +5,17 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './<%= name %>.component.html',
   styleUrls: ['./<%= name %>.component.scss']
 })
-export class <%= name %>Component implements OnInit {
-
+export class <%= classify(name) %>Component implements OnInit {
+  private static readonly DEFAULT_CONTENT: string = 'nothing to see here';
   public componentContent: string;
 
   public ngOnInit(): void {
+    <% if (undefined !== content && content.trim() !== '') { %>
     this.componentContent = '<%= content %>';
+  <% } else { %>
+    this.componentContent = <%= classify(name) %>Component.DEFAULT_CONTENT;
+  <% } %>
   }
 }
+
+
